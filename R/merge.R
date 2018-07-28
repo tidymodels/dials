3f2fc5fb329daeb6fcf0d56883c1d790ff80ac68
topepo <mxkuhn@gmail.com>
2018-07-28 16:58:14 -0400

    allow for reverse arg specification in merge

--- R/merge.R
+++ R/merge.R
@@ -3,17 +3,26 @@
 #' \pkg{parsnip} contains model objects that have consistent names with 
 #' \pkg{dials}. `merge` can be used to easily update any of the main parameters
 #' in a \pkg{parsnip} model. 
-#' @param x A \pkg{parsnip} model object (that has class `model_spec`). 
-#' @param y A parameter grid resulting from `regular_grid` or `random_grid`. 
+#' @param x,y A combination of one \pkg{parsnip} model object (that has class 
+#'  `model_spec`) and one parameter grid resulting from `regular_grid` or 
+#'  `random_grid`. As long as this combination is present, the assignment to 
+#'  `x` and `y` isn't restricted. 
 #' @param ... Not currently used. 
 #' @return An updated model object. 
 #' @importFrom utils getS3method
 #' @export
 merge.model_spec <- function(x, y, ...) {
-  if (!inherits(x, "model_spec"))
-    stop("The first argument should be a 'model_spec' object.", call. = FALSE)
-  if (!inherits(y, c("param_grid")))
-    stop("The first argument should be a 'param_grid' object.", call. = FALSE)
+  is_model_spec <- c(inherits(x, "model_spec"), inherits(y, "model_spec"))
+  is_param_grid <- c(inherits(x, "param_grid"), inherits(y, "param_grid"))
+  
+  if(
+    sum(is_model_spec) != 1 |
+    sum(is_param_grid) != 1 | 
+    (sum(is_param_grid) + sum(is_param_grid) != 2)
+  ) 
+    stop("`x` and `y` should contain one 'param_grid' object and one ", 
+         "'model_spec' object.", call. = FALSE)
+  
   upd_mth <- try(getS3method("update", class(x)[1]), silent = TRUE)
   if (inherits(upd_mth, "try-error"))
     stop("No `update` method for class '", class(x)[1], "'.",  call. = FALSE)
@@ -27,3 +36,7 @@ merge.model_spec <- function(x, y, ...) {
   param$object <- x
   do.call("update", param )
 }
+#' @export
+#' @rdname merge.model_spec
+merge.param_grid <- function(x, y, ...)
+  merge.model_spec(y, x, ...)
