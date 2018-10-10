1c0b9f4da7568ed5d59ab0e7089d1c878268e496
DavisVaughan <davis@rstudio.com>
2018-10-10 11:47:56 -0400

    Improve merge to automatically merge multi-row parameter grids with a single model_spec

--- R/merge.R
+++ R/merge.R
@@ -1,42 +1,74 @@
 #' Merge parameter grid values into a parsnip object
-#' 
-#' \pkg{parsnip} contains model objects that have consistent names with 
+#'
+#' \pkg{parsnip} contains model objects that have consistent names with
 #' \pkg{dials}. `merge` can be used to easily update any of the main parameters
-#' in a \pkg{parsnip} model. 
-#' @param x,y A combination of one \pkg{parsnip} model object (that has class 
-#'  `model_spec`) and one parameter grid resulting from `grid_regular` or 
-#'  `grid_random`. As long as this combination is present, the assignment to 
-#'  `x` and `y` isn't restricted. 
-#' @param ... Not currently used. 
-#' @return An updated model object. 
+#' in a \pkg{parsnip} model.
+#' @param x,y A combination of one \pkg{parsnip} model object (that has class
+#'  `model_spec`) and one parameter grid resulting from `grid_regular` or
+#'  `grid_random`. As long as this combination is present, the assignment to
+#'  `x` and `y` isn't restricted.
+#' @param ... Not currently used.
+#' @return A list containing updated model objects.
 #' @importFrom utils getS3method
 #' @export
 merge.model_spec <- function(x, y, ...) {
-  is_model_spec <- c(inherits(x, "model_spec"), inherits(y, "model_spec"))
-  is_param_grid <- c(inherits(x, "param_grid"), inherits(y, "param_grid"))
-  
-  if(
-    sum(is_model_spec) != 1 |
-    sum(is_param_grid) != 1 | 
-    (sum(is_param_grid) + sum(is_param_grid) != 2)
-  ) 
-    stop("`x` and `y` should contain one 'param_grid' object and one ", 
+
+  # x is known to be a model_spec
+  is_param_grid <- inherits(y, "param_grid")
+
+  if(!is_param_grid) {
+    stop("`x` and `y` should contain one 'param_grid' object and one ",
          "'model_spec' object.", call. = FALSE)
-  
-  upd_mth <- try(getS3method("update", class(x)[1]), silent = TRUE)
-  if (inherits(upd_mth, "try-error"))
-    stop("No `update` method for class '", class(x)[1], "'.",  call. = FALSE)
-  
+  }
+
+  actual_model_spec <- class(x)[1]
+
+  upd_mth <- try(getS3method("update", actual_model_spec), silent = TRUE)
+
+  if (inherits(upd_mth, "try-error")) {
+    stop(
+      "No `update` method for class '", actual_model_spec, "'.",
+      call. = FALSE
+    )
+  }
+
   mod_param <- names(x$args)
   param_names <- names(y)
   common <- intersect(mod_param, param_names)
-  if (length(common) == 0)
+
+  if (length(common) == 0) {
     return(x)
-  param <- as.list(y[, common])
-  param$object <- x
-  do.call("update", param )
+  }
+
+  # always return a list for type stability, even if nrow = 1
+  nrow_y <- nrow(y)
+  nrow_seq <- seq_len(nrow_y)
+  spec_list <- rlang::new_list(nrow_y)
+
+  param_obj <- list(object = x)
+
+  spec_list <- purrr::map(nrow_seq, ~{
+
+    param_lst <- as.list(y[.x, common, drop = FALSE])
+    param_lst <- c(param_lst, param_obj)
+    do.call(upd_mth, param_lst)
+
+  })
+
+  spec_list
 }
+
 #' @export
 #' @rdname merge.model_spec
-merge.param_grid <- function(x, y, ...)
+merge.param_grid <- function(x, y, ...) {
+
+  # x is known to be a param_grid
+  is_model_spec <- inherits(y, "model_spec")
+
+  if(!is_model_spec) {
+    stop("`x` and `y` should contain one 'param_grid' object and one ",
+         "'model_spec' object.", call. = FALSE)
+  }
+
   merge.model_spec(y, x, ...)
+}
