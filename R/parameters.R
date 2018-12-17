70862637edf62468bda9ae771fe5499921f40e88
DavisVaughan <davis@rstudio.com>
2018-12-17 09:56:29 -0500

    Document arguments to parameter generators. Lift the `trans` argument to be a user facing parameter

--- R/parameters.R
+++ R/parameters.R
@@ -3,6 +3,21 @@
 #' These are objects that can be used for modeling, especially in conjunction
 #'  with the \pkg{parsnip} package.
 #'
+#' @param range A two-element vector holding the smallest and largest possible
+#' values, respectively. If these cannot be set when the parameter is created,
+#' `unknown()` can be used instead of a particular value. If a transformation
+#' is specified, these should be in the _transformed units_. The default is
+#' an educated guess at the range of each parameter.
+#'
+#' @param values A vector of possible parameter values. For qualitative
+#' parameters, character and logical defaults have been set, but can be modified
+#' as needed.
+#'
+#' @param trans A `trans` object from the `scales` package, such as
+#' `scales::log10_trans()` or `scales::reciprocal_trans()`. If not provided,
+#' the default is used which matches the units used in `range`. If no
+#' transformation, `NULL`.
+#'
 #' @details
 #' These objects are pre-made parameter sets that are useful when the model is
 #'  based on trees or rules.
@@ -36,12 +51,12 @@
 #' @aliases tree_parameters
 #' @export
 #' @rdname tree_parameters
-mtry <- function(range = c(1L, unknown())) {
+mtry <- function(range = c(1L, unknown()), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(mtry = "# Randomly Selected Predictors"),
     finalize = get_p
   )
@@ -50,12 +65,12 @@ mtry <- function(range = c(1L, unknown())) {
 #' @export
 #' @rdname tree_parameters
 #' @importFrom scales log10_trans
-mtry_long <- function(range = c(0L, unknown())) {
+mtry_long <- function(range = c(0L, unknown()), trans = log10_trans()) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = log10_trans(),
+    trans = trans,
     label = c(mtry_long = "# Randomly Selected Predictors"),
     finalize = get_log_p
   )
@@ -63,12 +78,12 @@ mtry_long <- function(range = c(0L, unknown())) {
 
 #' @rdname tree_parameters
 #' @export
-trees <- function(range = c(1L, 2000L)) {
+trees <- function(range = c(1L, 2000L), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(trees = "# Trees"),
     finalize = NULL
   )
@@ -76,12 +91,12 @@ trees <- function(range = c(1L, 2000L)) {
 
 #' @rdname tree_parameters
 #' @export
-min_n <- function(range = c(2L, unknown())) {
+min_n <- function(range = c(2L, unknown()), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(min_n = "Minimal Node Size"),
     finalize = get_n_frac
   )
@@ -89,12 +104,12 @@ min_n <- function(range = c(2L, unknown())) {
 
 #' @rdname tree_parameters
 #' @export
-sample_size <- function(range = c(unknown(), unknown())) {
+sample_size <- function(range = c(unknown(), unknown()), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(sample_size = "# Observations Sampled"),
     finalize = get_n_frac_range
   )
@@ -102,12 +117,12 @@ sample_size <- function(range = c(unknown(), unknown())) {
 
 #' @rdname tree_parameters
 #' @export
-learn_rate <- function(range = c(unknown(), unknown())) {
+learn_rate <- function(range = c(unknown(), unknown()), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(learn_rate = "Learning Rate"),
     finalize = NULL
   )
@@ -116,12 +131,12 @@ learn_rate <- function(range = c(unknown(), unknown())) {
 
 #' @rdname tree_parameters
 #' @export
-loss_reduction <- function(range = c(unknown(), unknown())) {
+loss_reduction <- function(range = c(unknown(), unknown()), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(loss_reduction = "Minimum Loss Reduction"),
     finalize = NULL
   )
@@ -129,12 +144,12 @@ loss_reduction <- function(range = c(unknown(), unknown())) {
 
 #' @rdname tree_parameters
 #' @export
-tree_depth <- function(range = c(2L, 15L)) {
+tree_depth <- function(range = c(2L, 15L), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(tree_depth = "Tree Depth"),
     finalize = NULL
   )
@@ -153,12 +168,12 @@ prune <- function(values = c(TRUE, FALSE)) {
 
 #' @export
 #' @rdname tree_parameters
-Cp <- function(range = c(-10, -1)) {
+Cp <- function(range = c(-10, -1), trans = log10_trans()) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = log10_trans(),
+    trans = trans,
     label = c(Cp = "Cost-Complexity Parameter"),
     finalize = NULL
   )
@@ -170,6 +185,8 @@ Cp <- function(range = c(-10, -1)) {
 #'
 #' These are objects that can be used for modeling, especially in conjunction
 #'  with the \pkg{parsnip} package.
+#'
+#' @inheritParams mtry
 #'
 #' @details
 #' These objects are pre-made parameter sets that are useful when the model is
@@ -206,12 +223,12 @@ Cp <- function(range = c(-10, -1)) {
 #' @rdname para_parameters
 #' @export
 #' @export
-dropout <- function(range = c(0, 1)) {
+dropout <- function(range = c(0, 1), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, FALSE),
-    trans = NULL,
+    trans = trans,
     label = c(dropout = "Dropout Rate"),
     finalize = NULL
   )
@@ -219,12 +236,12 @@ dropout <- function(range = c(0, 1)) {
 
 #' @rdname para_parameters
 #' @export
-epochs <- function(range = c(1L, 1000L)) {
+epochs <- function(range = c(1L, 1000L), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = c(1L, 1000L),
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(epochs = "# Epochs"),
     finalize = NULL
   )
@@ -243,12 +260,12 @@ activation <- function(values = c("linear", "softmax", "relu", "elu")) {
 
 #' @rdname para_parameters
 #' @export
-mixture <- function(range = c(0, 1)) {
+mixture <- function(range = c(0, 1), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(mixture = "% lasso Penalty"),
     finalize = NULL
   )
@@ -256,12 +273,12 @@ mixture <- function(range = c(0, 1)) {
 
 #' @rdname para_parameters
 #' @export
-penalty <- function(range = c(-10, 0)) {
+penalty <- function(range = c(-10, 0), trans = log10_trans()) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = log10_trans(),
+    trans = trans,
     label = c(penalty = "Amount of Regularization"),
     finalize = NULL
   )
@@ -269,12 +286,12 @@ penalty <- function(range = c(-10, 0)) {
 
 #' @export
 #' @rdname para_parameters
-rbf_sigma <- function(range = c(-10, 0)) {
+rbf_sigma <- function(range = c(-10, 0), trans = log10_trans()) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = log10_trans(),
+    trans = trans,
     label = c(rbf_sigma = "Radial Basis Function sigma"),
     finalize = get_rbf_range
   )
@@ -282,12 +299,12 @@ rbf_sigma <- function(range = c(-10, 0)) {
 
 #' @export
 #' @rdname para_parameters
-prod_degree <- function(range = c(1L, 2L)) {
+prod_degree <- function(range = c(1L, 2L), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(prod_degree = "Degree of Interaction"),
     finalize = NULL
   )
@@ -295,12 +312,12 @@ prod_degree <- function(range = c(1L, 2L)) {
 
 #' @export
 #' @rdname para_parameters
-num_terms <- function(range = c(1L, unknown())) {
+num_terms <- function(range = c(1L, unknown()), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(num_terms = "# Model Terms"),
     finalize = get_p
   )
@@ -308,12 +325,12 @@ num_terms <- function(range = c(1L, unknown())) {
 
 #' @export
 #' @rdname para_parameters
-num_comp <- function(range = c(1L, unknown())) {
+num_comp <- function(range = c(1L, unknown()), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(num_comp = "# Components"),
     finalize = get_p
   )
@@ -321,12 +338,12 @@ num_comp <- function(range = c(1L, unknown())) {
 
 #' @export
 #' @rdname para_parameters
-cost <- function(range = c(-10, -1)) {
+cost <- function(range = c(-10, -1), trans = log2_trans()) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = log2_trans(),
+    trans = trans,
     label = c(cost = "Cost"),
     finalize = NULL
   )
@@ -334,12 +351,12 @@ cost <- function(range = c(-10, -1)) {
 
 #' @export
 #' @rdname para_parameters
-scale_factor <- function(range = c(-10, -1)) {
+scale_factor <- function(range = c(-10, -1), trans = log2_trans()) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = log2_trans(),
+    trans = trans,
     label = c(cost = "Scale Factor"),
     finalize = NULL
   )
@@ -347,12 +364,12 @@ scale_factor <- function(range = c(-10, -1)) {
 
 #' @export
 #' @rdname para_parameters
-margin <- function(range = c(0, .2)) {
+margin <- function(range = c(0, .2), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(cost = "Insensitivity Margin"),
     finalize = NULL
   )
@@ -360,12 +377,12 @@ margin <- function(range = c(0, .2)) {
 
 #' @export
 #' @rdname para_parameters
-degree <- function(range = c(1, 3)) {
+degree <- function(range = c(1, 3), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(degree = "Polynomial Degree"),
     finalize = NULL
   )
@@ -373,12 +390,12 @@ degree <- function(range = c(1, 3)) {
 
 #' @export
 #' @rdname para_parameters
-deg_free <- function(range = c(1, 5)) {
+deg_free <- function(range = c(1, 5), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(deg_free = "Degrees of Freedom"),
     finalize = NULL
   )
@@ -386,12 +403,12 @@ deg_free <- function(range = c(1, 5)) {
 
 #' @export
 #' @rdname para_parameters
-hidden_units <- function(range = c(1L, 10)) {
+hidden_units <- function(range = c(1L, 10), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(hidden_units = "# Hidden Units"),
     finalize = NULL
   )
@@ -400,12 +417,12 @@ hidden_units <- function(range = c(1L, 10)) {
 #' @export
 #' @rdname para_parameters
 #' @importFrom scales log2_trans
-batch_size <- function(range = c(unknown(), unknown())) {
+batch_size <- function(range = c(unknown(), unknown()), trans = log2_trans()) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = log2_trans(),
+    trans = trans,
     label = c(cost = "Batch Size"),
     finalize = get_batch_sizes
   )
@@ -429,6 +446,8 @@ prune_method <-
 #'
 #' These are objects that can be used for modeling, especially in conjunction
 #'  with the \pkg{parsnip} package.
+#'
+#' @inheritParams mtry
 #'
 #' @details
 #' These objects are pre-made parameter sets that are useful in a variety of
@@ -474,12 +493,12 @@ surv_dist <- function(values = c("weibull", "exponential", "gaussian",
 
 #' @export
 #' @rdname misc_parameters
-Laplace <- function(range = c(0, 3)) {
+Laplace <- function(range = c(0, 3), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     default = 0,
     label = c(Laplace = "Laplace Correction"),
     finalize = NULL
@@ -489,12 +508,12 @@ Laplace <- function(range = c(0, 3)) {
 
 #' @export
 #' @rdname misc_parameters
-neighbors <- function(range = c(1L, unknown())) {
+neighbors <- function(range = c(1L, unknown()), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(neighbors = "# Nearest Neighbors"),
     finalize = get_n_frac
   )
@@ -502,12 +521,12 @@ neighbors <- function(range = c(1L, unknown())) {
 
 #' @export
 #' @rdname misc_parameters
-dist_power <- function(range = c(1, 2)) {
+dist_power <- function(range = c(1, 2), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(neighbors = "Minkowski Distance Order")
   )
 }
@@ -515,12 +534,12 @@ dist_power <- function(range = c(1, 2)) {
 
 #' @export
 #' @rdname misc_parameters
-threshold <- function(range = c(0, 1)) {
+threshold <- function(range = c(0, 1), trans = NULL) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     default = 0.5,
     label = c(threshold = "Threshold"),
     finalize = NULL
@@ -534,6 +553,8 @@ threshold <- function(range = c(0, 1)) {
 #'
 #' These are objects that can be used for modeling, especially in conjunction
 #'  with the \pkg{textrecipes} package.
+#'
+#' @inheritParams mtry
 #'
 #' @details
 #' These objects are pre-made parameter sets that are useful in a variety of
@@ -561,12 +582,12 @@ threshold <- function(range = c(0, 1)) {
 
 #' @export
 #' @rdname text_parameters
-weight <- function(range = c(-10, 0)) {
+weight <- function(range = c(-10, 0), trans = log10_trans()) {
   new_quant_param(
     type = "double",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = log10_trans(),
+    trans = trans,
     label = c(weight = "Weight"),
     finalize = NULL
   )
@@ -601,12 +622,12 @@ token <- function(values = c("words", "characters", "character_shingle",
 
 #' @export
 #' @rdname text_parameters
-max_times <- function(range = c(1L, as.integer(10^5))) {
+max_times <- function(range = c(1L, as.integer(10^5)), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(max_times = "Maximum Token Frequency"),
     finalize = NULL
   )
@@ -615,12 +636,12 @@ max_times <- function(range = c(1L, as.integer(10^5))) {
 
 #' @export
 #' @rdname text_parameters
-min_times <- function(range = c(0L, 1000L)) {
+min_times <- function(range = c(0L, 1000L), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(min_times = "Minimum Token Frequency"),
     finalize = NULL
   )
@@ -629,12 +650,12 @@ min_times <- function(range = c(0L, 1000L)) {
 
 #' @export
 #' @rdname text_parameters
-max_tokens <- function(range = c(0L, as.integer(10^5))) {
+max_tokens <- function(range = c(0L, as.integer(10^5)), trans = NULL) {
   new_quant_param(
     type = "integer",
     range = range,
     inclusive = c(TRUE, TRUE),
-    trans = NULL,
+    trans = trans,
     label = c(min_times = "# Retained Tokens"),
     finalize = NULL
   )
