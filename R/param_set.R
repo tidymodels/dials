0c722aec35acd7d5218b9237a204766841971957
topepo <mxkuhn@gmail.com>
2019-08-09 21:41:43 -0400

    moved these functions (and model/recipe methods) to the tune package

--- R/param_set.R
+++ R/param_set.R
@@ -1,6 +1,7 @@
 #' Information on tuning parameters within an object
 #'
-#' @param x An object, such as a recipe, model specification, or workflow.
+#' @param x An object, such as a list of `param` objects or an actual `param`
+#' object.
 #' @param ... Only used for the `param` method so that multiple `param` objects
 #'can be passed to the function.
 #' @export
@@ -48,75 +49,6 @@ param_set.list <- function(x, ...) {
   )
 }
 
-#' @export
-#' @importFrom dplyr bind_rows
-#' @rdname param_set
-param_set.workflow <- function(x, ...) {
-  param_data <- param_set(x$fit$model$model)
-  if (any(names(x$pre) == "recipe")) {
-    param_data <-
-      dplyr::bind_rows(
-        param_data,
-        param_set(x$pre$recipe$recipe)
-      )
-  }
-  param_set_constr(
-    param_data$name,
-    param_data$id,
-    param_data$source,
-    param_data$component,
-    param_data$component_id,
-    param_data$object
-  )
-}
-
-#' @export
-#' @rdname param_set
-param_set.model_spec <- function(x, ...) {
-  all_args <- tunable(x)
-  tuning_param <- tune_args(x)
-
-  res <-
-    dplyr::inner_join(
-      tuning_param %>% dplyr::select(-tunable, -component_id),
-      all_args,
-      by = c("name", "source", "component")
-    ) %>%
-    mutate(object = map(call_info, eval_call_info))
-
-  param_set_constr(
-    res$name,
-    res$id,
-    res$source,
-    res$component,
-    res$component_id,
-    res$object
-  )
-
-}
-
-#' @export
-#' @rdname param_set
-param_set.recipe <- function(x, ...) {
-  all_args <- tunable(x)
-  tuning_param <- tune_args(x)
-  res <-
-    dplyr::inner_join(
-      tuning_param %>% dplyr::select(-tunable),
-      all_args,
-      by = c("name", "source", "component", "component_id")
-    ) %>%
-    mutate(object = map(call_info, eval_call_info))
-
-  param_set_constr(
-    res$name,
-    res$id,
-    res$source,
-    res$component,
-    res$component_id,
-    res$object
-  )
-}
 
 chr_check <- function(x) {
   if (!is.character(x)) {
@@ -268,6 +200,3 @@ eval_call_info <- function(x) {
   res
 }
 
-#' @importFrom utils globalVariables
-utils::globalVariables(c("component_id", "call_info", "object", "label",
-                         "id", "not_final", "component"))
