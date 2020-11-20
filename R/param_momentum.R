e9b00ec28a17482c17ba776d8a0829886287d6bc
Max Kuhn <mxkuhn@gmail.com>
2020-11-20 10:40:10 -0500

    added momentum

---
+++ R/param_momentum.R
@@ -0,0 +1,18 @@
+#' Gradient descent momentum parameter
+#'
+#' A useful parameter for neural network models using gradient descent
+#'
+#' @inheritParams Laplace
+#' @examples
+#' momentum()
+#' @export
+momentum <- function (range = c(0, 1), trans = NULL)  {
+  new_quant_param(
+    type = "double",
+    range = range,
+    inclusive = c(TRUE, TRUE),
+    trans = trans,
+    label = c(momentum = "Gradient Descent Momentum"),
+    finalize = NULL
+  )
+}
