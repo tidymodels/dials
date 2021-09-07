a908be3b893758f0e085495df37b0ffe027ca7e4
Hannah Frick <hannah@rstudio.com>
2021-09-07 21:39:47 +0100

    add `extract_dials_parameter()` and soft-deprecate `pull_dials_object()`

--- R/extract_dials_parameter.R
+++ R/extract_dials_parameter.R
@@ -1,6 +1,6 @@
 #' Return a dials parameter object associated with parameters
 #'
-#' `pull_dials_object()` can extract a single `dials` parameter object from
+#' `extract_dials_parameter()` can extract a single `dials` parameter object from
 #' different types of objects (e.g. parameter sets, recipes, etc.).
 #' @param x The results of a call to [parameters()], a recipe, model
 #' specification, or workflow.
@@ -9,21 +9,21 @@
 #' @return A `dials` parameter object.
 #' @examples
 #' glmn_param <- parameters(lambda = penalty(), mixture())
-#' pull_dials_object(glmn_param, "lambda")
+#' extract_dials_parameter(glmn_param, "lambda")
 #'
 #' # fails:
-#' # pull_dials_object(glmn_param, "penalty")
+#' # extract_dials_parameter(glmn_param, "penalty")
 #' @export
-pull_dials_object <- function(x, id, ...) {
-  UseMethod("pull_dials_object")
+extract_dials_parameter <- function(x, id, ...) {
+  UseMethod("extract_dials_parameter")
 }
 
 #' @export
-#' @rdname pull_dials_object
-pull_dials_object.parameters <- function(x, id, ...) {
+#' @rdname extract_dials_parameter
+extract_dials_parameter.parameters <- function(x, id, ...) {
   dots <- rlang::quos(...)
   if (!rlang::is_empty(dots))
-    rlang::abort("The `...` are not used with `pull_dials_object()`.")
+    rlang::abort("The `...` are not used with `extract_dials_parameter()`.")
   if (any(rlang::is_missing(id)) ||
       any(!is.character(id)) ||
       length(id) != 1 ||
@@ -40,20 +40,79 @@ pull_dials_object.parameters <- function(x, id, ...) {
 }
 
 #' @export
+#' @rdname extract_dials_parameter
+extract_dials_parameter.recipe <- function(x, id, ...) {
+  extract_dials_parameter(parameters(x), id)
+}
+
+#' @export
+#' @rdname extract_dials_parameter
+extract_dials_parameter.model_spec <- function(x, id, ...) {
+  extract_dials_parameter(parameters(x), id)
+}
+
+#' @export
+#' @rdname extract_dials_parameter
+extract_dials_parameter.workflow <- function(x, id, ...) {
+  extract_dials_parameter(parameters(x), id)
+}
+
+#' Return a dials parameter object associated with parameters
+#'
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#' This function has been deprecated in favor of `extract_dials_parameter()`.
+#'
+#' `pull_dials_object()` can extract a single `dials` parameter object from
+#' different types of objects (e.g. parameter sets, recipes, etc.).
+#'
+#' @param x The results of a call to [parameters()], a recipe, model
+#' specification, or workflow.
+#' @param id A single string for the `id` of the parameter.
+#' @param ... Not currently used.
+#' @return A `dials` parameter object.
+#' @keywords internal
+#' @examples
+#' glmn_param <- parameters(lambda = penalty(), mixture())
+#' pull_dials_object(glmn_param, "lambda")
+#'
+#' # fails:
+#' # pull_dials_object(glmn_param, "penalty")
+#' #->
+#' glmn_param <- parameters(lambda = penalty(), mixture())
+#' extract_dials_parameter(glmn_param, "lambda")
+#'
+#' # fails:
+#' # extract_dials_parameter(glmn_param, "penalty")
+#' @export
+pull_dials_object <- function(x, id, ...) {
+  UseMethod("pull_dials_object")
+}
+
+#' @export
+#' @rdname pull_dials_object
+pull_dials_object.parameters <- function(x, id, ...) {
+  lifecycle::deprecate_soft("0.0.10", "pull_dials_object()", "extract_dials_parameter()")
+  extract_dials_parameter(x, id, ...)
+}
+
+#' @export
 #' @rdname pull_dials_object
 pull_dials_object.recipe <- function(x, id, ...) {
-  pull_dials_object(parameters(x), id)
+  lifecycle::deprecate_soft("0.0.10", "pull_dials_object()", "extract_dials_parameter()")
+  extract_dials_parameter(x, id, ...)
 }
 
 #' @export
 #' @rdname pull_dials_object
 pull_dials_object.model_spec <- function(x, id, ...) {
-  pull_dials_object(parameters(x), id)
+  lifecycle::deprecate_soft("0.0.10", "pull_dials_object()", "extract_dials_parameter()")
+  extract_dials_parameter(x, id, ...)
 }
 
 #' @export
 #' @rdname pull_dials_object
 pull_dials_object.workflow <- function(x, id, ...) {
-  pull_dials_object(parameters(x), id)
+  lifecycle::deprecate_soft("0.0.10", "pull_dials_object()", "extract_dials_parameter()")
+  extract_dials_parameter(x, id, ...)
 }
-
--- tests/testthat/test-extract_dials_parameter.R
+++ tests/testthat/test-extract_dials_parameter.R
@@ -1,19 +1,69 @@
 
-context("pulling dials objects")
 load(test_path("test_object.RData"))
-
-# ------------------------------------------------------------------------------
 
 test_that('regular usage', {
   # perhaps move these to extratests
-  # expect_equal(pull_dials_object(mod, "lambda"), penalty())
-  # expect_equal(pull_dials_object(mod, "mixture"), mixture(c(0.05, 1)))
-  # expect_equal(pull_dials_object(rec, "wts"), spline_degree(c(1, 15)))
-  # expect_equal(pull_dials_object(rec, "disp"), spline_degree(c(1, 15)))
-  # expect_equal(pull_dials_object(wflow, "lambda"), penalty())
-  # expect_equal(pull_dials_object(wflow, "mixture"), mixture(c(0.05, 1)))
-  # expect_equal(pull_dials_object(wflow, "wts"), spline_degree(c(1, 15)))
-  # expect_equal(pull_dials_object(wflow, "disp"), spline_degree(c(1, 15)))
+  # expect_equal(extract_dials_parameter(mod, "lambda"), penalty())
+  # expect_equal(extract_dials_parameter(mod, "mixture"), mixture(c(0.05, 1)))
+  # expect_equal(extract_dials_parameter(rec, "wts"), spline_degree(c(1, 15)))
+  # expect_equal(extract_dials_parameter(rec, "disp"), spline_degree(c(1, 15)))
+  # expect_equal(extract_dials_parameter(wflow, "lambda"), penalty())
+  # expect_equal(extract_dials_parameter(wflow, "mixture"), mixture(c(0.05, 1)))
+  # expect_equal(extract_dials_parameter(wflow, "wts"), spline_degree(c(1, 15)))
+  # expect_equal(extract_dials_parameter(wflow, "disp"), spline_degree(c(1, 15)))
+
+  expect_equal(extract_dials_parameter(mod_param, "lambda"), penalty())
+  expect_equal(extract_dials_parameter(mod_param, "mixture"), mixture(c(0.05, 1)))
+  expect_equal(extract_dials_parameter(rec_param, "wts"), spline_degree(c(1, 15)))
+  expect_equal(extract_dials_parameter(rec_param, "disp"), spline_degree(c(1, 15)))
+  expect_equal(extract_dials_parameter(wflow_param, "lambda"), penalty())
+  expect_equal(extract_dials_parameter(wflow_param, "mixture"), mixture(c(0.05, 1)))
+  expect_equal(extract_dials_parameter(wflow_param, "wts"), spline_degree(c(1, 15)))
+  expect_equal(extract_dials_parameter(wflow_param, "disp"), spline_degree(c(1, 15)))
+})
+
+test_that('bad arguments', {
+  expect_error(
+    extract_dials_parameter(mod_param, "lambdas"),
+    "No parameter exists with id 'lambdas'"
+  )
+  expect_error(
+    extract_dials_parameter(mod_param),
+    "Please supply a single 'id' string"
+  )
+  expect_error(
+    extract_dials_parameter(mod_param, 1),
+    "Please supply a single 'id' string"
+  )
+  expect_error(
+    extract_dials_parameter(mod_param, 1:2),
+    "Please supply a single 'id' string"
+  )
+  expect_error(
+    extract_dials_parameter(mod_param, letters[1:2]),
+    "Please supply a single 'id' string"
+  )
+  expect_error(
+    extract_dials_parameter(mod_param, NA_character_),
+    "Please supply a single 'id' string"
+  )
+  expect_error(
+    extract_dials_parameter(mod_param, ""),
+    "Please supply a single 'id' string"
+  )
+})
+
+# ------------------------------------------------------------------------------
+
+load(test_path("test_object.RData"))
+
+test_that("`pull_dials_object() is deprecated", {
+  withr::local_options(lifecycle_verbosity = "warning")
+  expect_warning(pull_dials_object(mod_param, "lambda"))
+})
+
+test_that('regular usage', {
+  withr::local_options(lifecycle_verbosity = "quiet")
 
   expect_equal(pull_dials_object(mod_param, "lambda"), penalty())
   expect_equal(pull_dials_object(mod_param, "mixture"), mixture(c(0.05, 1)))
@@ -28,6 +78,8 @@ test_that('regular usage', {
 # ------------------------------------------------------------------------------
 
 test_that('bad arguments', {
+  withr::local_options(lifecycle_verbosity = "quiet")
+
   expect_error(
     pull_dials_object(mod_param, "lambdas"),
     "No parameter exists with id 'lambdas'"
