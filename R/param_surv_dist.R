86db57d98b49e5fa3e8b39497f543c16d43b754b
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2021-11-10 14:34:49 -0500

    Add flexsurv survival distributions and knots parameter

--- R/param_surv_dist.R
+++ R/param_surv_dist.R
@@ -1,13 +1,14 @@
 #' Parametric distributions for censored data
 #'
 #' @param values A character string of possible values. See `values_surv_dist`
-#'  in examples below.
+#'  or `values_flexsurv_dist` in examples below.
 #'
 #' @details
 #' This parameter is used in `parsnip:::surv_reg()`.
 #' @examples
 #' values_surv_dist
 #' surv_dist()
+#' surv_dist(values_flexsurv_dist)
 #' @export
 surv_dist <- function(values = values_surv_dist) {
   new_qual_param(
@@ -23,3 +24,7 @@ surv_dist <- function(values = values_surv_dist) {
 values_surv_dist <- c("weibull", "exponential", "gaussian",
                       "logistic", "lognormal", "loglogistic")
 
+#' @rdname surv_dist
+#' @export
+values_flexsurv_dist <- c("gengamma", "genf", "weibull", "gamma",
+                          "exponential", "llogis", "lognormal", "gompertz")
