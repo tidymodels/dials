4cb49370f66f84fc8c434d55e65904a96a087890
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2021-11-24 12:03:03 -0500

    Rename surv_scale to surv_link

--- R/param_surv_scale.R
+++ R/param_surv_scale.R
@@ -1,23 +1,23 @@
-#' Survival Model Scale
+#' Survival Model Link Function
 #'
-#' @param values A character string of possible values. See `values_surv_scale`
+#' @param values A character string of possible values. See `values_surv_link`
 #'   in examples below.
 #'
 #' @details
 #' This parameter is used in `parsnip::set_engine('flexsurvspline')`.
 #' @examples
-#' values_surv_scale
-#' surv_scale()
+#' values_surv_link
+#' surv_link()
 #' @export
-surv_scale <- function(values = values_surv_scale) {
+surv_link <- function(values = values_surv_link) {
   new_qual_param(
     type = "character",
     values = values,
-    label = c(surv_scale = "Survival Scale"),
+    label = c(surv_link = "Survival Link"),
     finalize = NULL
   )
 }
 
-#' @rdname surv_scale
+#' @rdname surv_link
 #' @export
-values_surv_scale <- c("hazard", "odds", "normal")
+values_surv_link <- c("hazard", "odds", "normal")
