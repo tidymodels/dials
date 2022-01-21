0ab93627056baa020619083390bae825477a39df
Hannah Frick <hannah@rstudio.com>
2022-01-21 18:05:37 +0000

    use lifecycle

--- R/dials-package.R
+++ R/dials-package.R
@@ -32,5 +32,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
