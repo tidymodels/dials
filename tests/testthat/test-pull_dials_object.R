c26d514df265a9ce173ffd4a78383e58054160bb
Hannah Frick <hannah@rstudio.com>
2022-04-05 17:53:30 +0100

    remove test objects
    they contain old `scales::trans` objects which break with the upcoming version of scales

--- tests/testthat/test-pull_dials_object.R
+++ tests/testthat/test-pull_dials_object.R
@@ -1,13 +1,27 @@
 
-load(test_path("data", "test_object.RData"))
-
 test_that("pull_dials_object is deprecated", {
+  mod_param <- parameters(list("lambda" = penalty(), mixture(range = c(0.05, 1))))
   expect_snapshot(pull_dials_object(mod_param, "mixture"))
 })
 
 test_that("regular usage", {
   withr::local_options(lifecycle_verbosity = "quiet")
 
+  mod_param <- parameters(list("lambda" = penalty(), mixture(range = c(0.05, 1))))
+  rec_param <- parameters(
+    list(
+      "wts" = spline_degree(range = c(1, 15)),
+      "disp" = spline_degree(range = c(1, 15))
+    )
+  )
+  wflow_param <- parameters(
+    list(
+      "wts" = spline_degree(range = c(1, 15)),
+      "disp" = spline_degree(range = c(1, 15)),
+      "lambda" = penalty(),
+      mixture(range = c(0.05, 1))
+    )
+  )
   # perhaps move these to extratests
   # expect_equal(pull_dials_object(mod, "lambda"), penalty())
   # expect_equal(pull_dials_object(mod, "mixture"), mixture(c(0.05, 1)))
@@ -30,6 +44,8 @@ test_that("regular usage", {
 
 test_that("bad arguments", {
   withr::local_options(lifecycle_verbosity = "quiet")
+
+  mod_param <- parameters(list("lambda" = penalty(), mixture(range = c(0.05, 1))))
 
   expect_snapshot(error = TRUE, pull_dials_object(mod_param, "lambdas"))
   expect_snapshot(error = TRUE, pull_dials_object(mod_param))
