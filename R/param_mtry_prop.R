57bdedf67ba45eb723c9ca357ad59b9bbc0c7cd3
simonpcouch <simonpatrickcouch@gmail.com>
2022-05-24 13:15:00 -0400

    improve docs re: `mtry` vs `mtry_prop`

--- R/param_mtry_prop.R
+++ R/param_mtry_prop.R
@@ -9,13 +9,25 @@
 #' `range` element of the object is always converted to a list with elements
 #' "lower" and "upper".
 #'
-#' @details This argument is a variation on [mtry()] where the value is
+#' @section Interpretation:
+#'
+#' [mtry_prop()] is a variation on [mtry()] where the value is
 #' interpreted as the _proportion_ of predictors that will be randomly sampled
-#' at each split rather than the _count_. It is typically only used to
-#' accommodate engines that take in this argument as a proportion,
-#  --- TODO: link to bonsai here, eventually ---
-#' such as `lightgbm` (as `feature_fraction`) and
-#' [`xgboost`][parsnip::xgb_train()] (as `colsample_by*`).
+#' at each split rather than the _count_.
+#'
+#' This parameter is not intended for use in accommodating engines that take in
+#' this argument as a proportion; `mtry` is often a main model argument
+#' rather than an engine-specific argument, and thus should not have an
+#' engine-specific interface.
+#'
+#' When wrapping modeling engines that interpret `mtry` in its sense as a
+#' proportion, use the [mtry()] parameter in [parsnip::set_model_arg()] and
+#' process the passed argument in an internal wrapping function as
+#' `mtry / number_of_predictors`. In addition, introduce a logical argument
+#' `counts` to the wrapping function, defaulting to `TRUE`, that indicates
+#' whether to interpret the supplied argument as a count rather than a proportion.
+#'
+#' For an example implementation, see [parsnip::xgb_train()].
 #'
 #' @seealso mtry, mtry_long
 #'
